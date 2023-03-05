4b7b1cf7331deddb92410c9c3f3b76316138e5bb
AhmedAdel74 <ahmed.elsherif7474@gmail.com>
2023-03-05 02:50:29 +0200

    job-templates added

--- job/views.py
+++ job/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
+
 def jobs_list(request):
-    pass
+
+    jobs_list = Job.objects.all()
+    context = {'jobs': jobs_list}
+    return render(request,'job/jobs_list.html',context)
+
+
 
 def job_details(request, id):
     pass
