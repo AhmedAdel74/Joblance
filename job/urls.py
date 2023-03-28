78a0d31baa1db643f67e3de22a2fe930f8c3d094
AhmedAdel74 <ahmed.elsherif7474@gmail.com>
2023-03-29 01:49:47 +0200

    adding job filters

--- job/urls.py
+++ job/urls.py
@@ -1,5 +1,7 @@
 from django.urls import  path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'job'
 urlpatterns = [
@@ -7,4 +9,7 @@ urlpatterns = [
     path('add', views.add_job, name='add_job'),
     path('<str:slug>', views.job_details, name='job_details'),
     
-]
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
