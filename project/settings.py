91679035eb163c24ea432cd114bf1d1bf868e1dd
Lilly5555 <alaamostafamoati@gmail.com>
2023-04-03 03:14:14 +0200

    adding home and index pages

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #my apps
     'job',
+    'pages',
     'bootstrap4',
     'django_filters',
 ]
