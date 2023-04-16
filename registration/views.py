4c56bab1ced01028452cf2ce7945cfa1e990002d
Taher Elkady <70442105+elkadytaher@users.noreply.github.com>
2023-04-16 15:11:23 +0200

    Improving User Registration and Login Experience by Addressing Common Loopholes and Adding Error Messaging.
    Adding the login and registration with closing some loopholes such as registration without the username or email, and also adding that it is possible to enter and write in the username field either the username or using the email, and also adding error messages to the site

--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import render,HttpResponse,redirect
 from .models import Login
 from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 # @login_required(login_url='login')
@@ -15,12 +15,21 @@ def SignupPage(request):
         pass1=request.POST.get('password1')
         pass2=request.POST.get('password2')
 
-        if pass1!=pass2:
-            return HttpResponse("Your password and confirm password do not match!!")
+        if uname=="":
+            messages.error(request, "Username is required.")
+        elif pass1=="":
+            messages.error(request, "Password is required.")
+        elif email=="":
+            messages.error(request, "email is required.")
+        elif checkExistUser(request)==True:
+            messages.info(request, f"We regret to inform you that the selected username or email, {uname} , is already registered in our system. We kindly request that you choose a different username and email address.")
+        elif pass1!=pass2:
+            messages.warning(request, "Your password and confirm password do not match!!")
         else:
 
             my_user=Login(username=uname,email=email,password=pass1)
             my_user.save()
+            messages.success(request, "The registration on the site was successful.")
             return redirect('registration:signin')
         
 
@@ -35,15 +44,28 @@ def LoginPage(request):
 
         # loop through all User objects in the Login module
         for user in Login.objects.all():
-            if user.username == username and user.password == password:
+            if (user.username == username or user.email==username) and user.password == password :#and (username != "" or password!="")
             # username and password exist, so return a success response
+                messages.success(request, "Authentication successful. You are now logged in to the website.")
                 return redirect('pages:home')
     
             # username and/or password do not exist, so return an error response
-        return HttpResponse("Username or password is incorrect!")
+        messages.warning(request, f"Authentication failed due to either an incorrect username or password, or the non-existence of a user with the username '{username}'.")
+        return render(request, 'registration\login.html')
     else:
         return render(request, 'registration\login.html')
     
 def LogoutPage(request):
     logout(request)
     return redirect('login')
+
+def checkExistUser(request):
+    username = request.POST['username']
+    email = request.POST['email']
+    for user in Login.objects.all():
+            if user.username == username or user.email == email:
+            # username and password exist, so return a success response
+                return True
+    
+            # username and/or password do not exist, so return an error response
+    return False
