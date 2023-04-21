6a43c83304bf345052428ce648fbf784f5b82be6
Ahmed Sharaf Mohammed Hassan <Ahmed.Sharaf.Mohammed.Hassan@gmail.com>
2023-04-21 03:32:13 +0200

    New

--- features/views.py
+++ features/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Craftsmen
 
 # Create your views here.
 def members(request):
     return render(request, 'features/members.html')
+
+def craftsmen(request):
+
+    craftsmen_list = Craftsmen.objects.all()
+
+    context = {'list': craftsmen_list,
+               }
+    return render(request, 'features/craftsmen.html', context)
