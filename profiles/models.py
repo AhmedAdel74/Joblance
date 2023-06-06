478e6bb4e1a5851a45d96a19d1334c8fc4923fab
Ahmed Sharaf Mohammed Hassan <Ahmed.Sharaf.Mohammed.Hassan@gmail.com>
2023-06-06 12:27:21 +0300

    Recommendation

--- profiles/models.py
+++ profiles/models.py
@@ -153,4 +153,3 @@ class Recommendation_Model (models.Model):
                 older_search_words = search_words[5:]
                 for word in older_search_words:
                     word.delete()
-                    
