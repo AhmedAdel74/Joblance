65579344ee4b82e9aaf466a4ed3be101ea6c5b57
Ahmed Adel <ahmed.elsherif7474@gmail.com>
2023-06-09 10:41:57 +0300

    updates #2

--- pages/views.py
+++ pages/views.py
@@ -5,13 +5,17 @@ from django.conf import settings
 from django.shortcuts import redirect
 from django.contrib.auth import logout
 from job.models import Job
+from features.models import Craftsmen
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 def index(request):
+    craftsmen = Craftsmen.objects.all()[:6]
+    top_rate = Craftsmen.get_top_rated_craftsmen()[:6]
     jobs_list = Job.objects.all()[:6]
     context = {'jobs': jobs_list,
+               'workers': top_rate,
                }
     return render(request, 'pages/index.html', context)
 
